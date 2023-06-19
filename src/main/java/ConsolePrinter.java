64b7bd9d351d1031004d3fc8a9ac6486c24dc16c
Michael Urisman <michael.urisman@gmail.com>
2023-06-19 14:05:40 +0300

    Changed type for intValue to Integer instead of int

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,6 +1,6 @@
 public class ConsolePrinter {
     public Integer printValue() {
-        int intValue = 1000;
+        Integer intValue = 1000;
         String formattedStrValue = String.format("---<%d>---", intValue);
         System.out.println(formattedStrValue);
         return intValue;
